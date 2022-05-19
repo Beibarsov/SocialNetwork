d65ac39193f4e46ce042f8a79cea94a225e524f5
Beibarsov <BarsStudio@gmail.com>
2022-05-19 18:34:05 +0800

    Демо-интерфейс

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+namespace SocialNetwork
+{
+    class Program
+    {
+        public static UserService userService = new UserService();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в социальную сеть");
+            while (true)
+            {
+                Console.WriteLine("Для регистрации пользователя введите имя:");
+                string firsName = Console.ReadLine();
+                Console.WriteLine("Для регистрации пользователя введите фамилию:");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Для регистрации пользователя введите пароль:");
+                string password = Console.ReadLine();
+                Console.WriteLine("Для регистрации пользователя введите почту:");
+                string email = Console.ReadLine();
+
+
+                UserRegistrationDate userRegistrationDate = new UserRegistrationDate()
+                {
+                    FirstName = firsName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(userRegistrationDate);
+                    Console.WriteLine("Регистрация успешно совершена");
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Введите корректное значение ");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка при регистрации");
+                }
+            }
+            
+        }
+
+    }
+
+
+
+
+}
+
+
+
